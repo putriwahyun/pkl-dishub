8f223cd0699e32d3d3b8da01496824af1bbf8197
alyafaizah <alya.faizah01@gmail.com>
2022-01-07 13:10:10 +0700

    tampilan awal janji temu

---
+++ calontamu.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Buku Tamu Dishub</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="asset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="asset/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Head -->
+        <div class="head text-center">
+            <img src="asset/img/logo.png" width="250">
+            <h2 class="text-white"><b>Janji Temu Dinas Perhubungan<br>Kota Malang</b></h2>
+        </div>
+        <!-- end Head -->
+
+        <div class="row mt-4">
+            <div class="col-lg-4 mb-3 ml-8"></div>
+            <div class="card shadow bg-gradient-light">
+                <div class="card-body" style="width:400px;">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">Selamat Datang di Dinas Perhubungan Kota Malang</h1>
+                            <p>Silahkan Isi Nama Anda Sebelum Membuat Janji Temu</p>
+                        </div>
+                        <form class="user" method="post" action="">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" name="user" placeholder="Nama Lengkap">
+                            </div>
+                            <div >
+                                <button class="btn btn-primary btn-user btn-block" type="submit" name="submit" >Mulai</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="asset/vendor/jquery/jquery.min.js"></script>
+        <script src="asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="asset/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
