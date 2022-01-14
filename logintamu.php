6bf7d40ebaa2dec59fc4fc5e24fc4c6419b321ef
nickenoctaviana <octaviananicken@gmail.com>
2022-01-14 13:44:44 +0700

    logintamu baru

--- logintamu.php
+++ logintamu.php
@@ -26,129 +26,33 @@
         <div class="container px-4 px-lg-5 h-100">
             <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                 <div class="col-lg-8 align-self-end">
-                    <h1 class="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
-                    <br>
-                    <br>
-                    <br>
+                    <h1 class="text-white font-weight-bold">Dinas Perhubungan<br>Kota Malang</h1>
                     <br>
                 </div>
                 <div class="col-lg-8 align-self-baseline">
-                    <a class="btn btn-xl" style="background-color: #548CFF;" href="#about">Find Out More</a>
+                    <a class="btn btn-xl" style="background-color: #548CFF; color:white;" href="#about">Find Out More</a>
                 </div>
             </div>
         </div>
     </header>
     <!-- About-->
-    <section class="page-section bg-primary" id="about">
-        <div class="row mt-2">
-            <div class="col-lg-4 ml-8"></div>
-            <div class="card shadow bg-gradient-light" style="width:380px;height:350px;">
-                <div class="card-body" style="width:380px;height:350px; text-align:center;">
-                    <div class="p-3">
-                        <div class="text-center">
-                            <p>Silahkan Isi Nama Anda Sebelum Membuat Janji Temu</p>
-                        </div>
-                        <form class="user" method="post" action="">
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-user" name="user" placeholder="Nama Lengkap">
-                            </div>
-                            <div>
-                                <button class="btn btn-primary btn-user btn-block" type="submit" name="submit" style="margin: 10px;">Masuk</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Contact-->
-    <section class="page-section" id="contact">
+    <section class="page-section" style="background-color: #548CFF;" id="about">
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-lg-8 col-xl-6 text-center">
-                    <h2 class="mt-0">Let's Get In Touch!</h2>
-                    <hr class="divider" />
-                    <p class="text-muted mb-5">Ready to start your next project with us? Send us a messages and we will get back to you as soon as possible!</p>
-                </div>
-            </div>
-            <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
-                <div class="col-lg-6">
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- * * SB Forms Contact Form * *-->
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- This form is pre-integrated with SB Forms.-->
-                    <!-- To make this form functional, sign up at-->
-                    <!-- https://startbootstrap.com/solution/contact-forms-->
-                    <!-- to get an API token!-->
-                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                        <!-- Name input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                            <label for="name">Full name</label>
-                            <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                        </div>
-                        <!-- Email address input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                            <label for="email">Email address</label>
-                            <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                            <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                        </div>
-                        <!-- Phone number input-->
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                            <label for="phone">Phone number</label>
-                            <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                        </div>
-                        <!-- Message input-->
-                        <div class="form-floating mb-3">
-                            <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
-                            <label for="message">Message</label>
-                            <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                        </div>
-                        <!-- Submit success message-->
-                        <!---->
-                        <!-- This is what your users will see when the form-->
-                        <!-- has successfully submitted-->
-                        <div class="d-none" id="submitSuccessMessage">
-                            <div class="text-center mb-3">
-                                <div class="fw-bolder">Form submission successful!</div>
-                                To activate this form, sign up at
-                                <br />
-                                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                            </div>
-                        </div>
-                        <!-- Submit error message-->
-                        <!---->
-                        <!-- This is what your users will see when there is-->
-                        <!-- an error submitting the form-->
-                        <div class="d-none" id="submitErrorMessage">
-                            <div class="text-center text-danger mb-3">Error sending message!</div>
-                        </div>
-                        <!-- Submit Button-->
-                        <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
-                    </form>
-                </div>
-            </div>
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-lg-4 text-center mb-5 mb-lg-0">
-                    <i class="bi-phone fs-2 mb-3 text-muted"></i>
-                    <div>+1 (555) 123-4567</div>
+                <div class="col-lg-8 text-center">
+                    <p class="text-white-75 mb-4">Silahkan Isi Nama Anda Sebelum Membuat Janji Temu</p>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                        <label for="name">Nama Lengkap</label>
+                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                    </div>
+                    <br>
+                    <a class="btn btn-light btn-xl" style="color:#548CFF" href="tamu.php">Mulai</a>
                 </div>
             </div>
         </div>
     </section>
     <!-- Footer-->
-    <footer class="bg-light py-5">
-        <div class="container px-4 px-lg-5">
-            <div class="small text-center text-muted">Copyright &copy; 2021 - Company Name</div>
-        </div>
-
-        <div class="container px-4 px-lg-5 pt-2">
-            <div class="small text-center text-muted">Distributed By <a href="https://themewagon.com/">Themewagon</a>
-            </div>
-        </div>
-    </footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- SimpleLightbox plugin JS-->
